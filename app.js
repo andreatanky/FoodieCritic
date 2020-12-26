b29b1feed8f59493330f84d971680e5cec4fb705
Andrea <andreaxtan611@gmail.com>
2020-12-26 23:17:04 +0800

    Update v3.1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ if (process.env.NODE_ENV !== "production") {
     require('dotenv').config();
 }
 
-const dbUrl = 'mongodb://localhost:27017/foodiecritic';
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/foodiecritic';
 
 const express = require('express');
 const path = require('path');
@@ -58,9 +58,11 @@ app.use(mongoSanitize({
     replaceWith: '_'
 }));
 
+const secret = process.env.SECRET || 'thisshouldbeabettersecret';
+
 const store = new MongoDBStore({
     url: dbUrl,
-    secret: 'thisshouldbeabettersecret',
+    secret,
     touchAfter: 24 * 60 * 60
 });
 
@@ -71,7 +73,7 @@ store.on("error", function (e) {
 const sessionconfig = {
     store,
     name: 'cookiemonster',
-    secret: 'thisshouldbeabettersecret',
+    secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
